5cab2c3a01f42d230288cf6c5df5a30386a5ece3
yoon-bob <yyss6188@planfit.ai>
2024-11-10 15:48:35 +0900

    코인 가격 안 가져오는 문제 고침

--- app/data_fetcher.py
+++ app/data_fetcher.py
@@ -19,9 +19,14 @@ def get_all_coins():
 
 def fetch_coin_data(coins):
     try:
-        markets = ','.join([coin.market for coin in coins])
-        url = f"https://api.upbit.com/v1/ticker?markets={markets}"
-        response = requests.get(url)
+        all_get_url = "https://api.upbit.com/v1/market/all?is_details=true"
+        response = requests.get(all_get_url)
+        response.raise_for_status()
+        all_data = response.json()
+        markets = ','.join([data['market'] for data in all_data])
+        url = f"https://api.upbit.com/v1/ticker"
+        response = requests.get(url, params={"markets": markets})
+        print(response.json())
         response.raise_for_status()
         return response.json()
     except Exception as e:
