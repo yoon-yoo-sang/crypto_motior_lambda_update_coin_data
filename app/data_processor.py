5517a4844345f3803db6e489d016ab550af45fc6
yoon-bob <yyss6188@planfit.ai>
2024-12-11 23:19:36 +0900

    도파민 지수 그럴듯하게 수정

--- app/data_processor.py
+++ app/data_processor.py
@@ -1,11 +1,14 @@
-def process_data(data):
-    volatility = abs(data['signed_change_rate'])
-    volume_score = data['acc_trade_price_24h']
-    dopamine_index = volatility * 0.5 + volume_score * 0.5
+def process_data(data, max_volatility, max_volume_score):
+    normalized_volatility = abs(data['signed_change_rate']) / max_volatility
+    normalized_volume_score = data['acc_trade_price_24h'] / max_volume_score
+
+    raw_dopamine_index = normalized_volatility * 0.5 + normalized_volume_score * 0.5
+
+    dopamine_index = raw_dopamine_index * 100
 
     processed_data = {
         **data,
-        'dopamine_index': dopamine_index,
+        'dopamine_index': round(dopamine_index, 2),  # 소수점 2자리로 반올림
         'rsi': 0,
         'macd': 0,
     }
